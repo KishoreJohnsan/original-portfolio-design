36613989ff1ec042bad8d7618216d2da57176e68
KishoreJohnsan <kishorejohnsan.s@gmail.com>
2020-04-25 18:40:59 +0530

    small changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import { MDCRipple } from "@material/ripple";
 import { MDCSwitch } from "@material/switch";
+import './main.js';
 
 const switchControl = new MDCSwitch(document.querySelector(".mdc-switch"));
 const ripple = new MDCRipple(document.querySelector(".mdc-button"));
+
+
 
 
